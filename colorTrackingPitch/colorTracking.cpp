4f7ba0a50f1c0510014efe207304e259132c2c1f
Paul Wienk <paulwienk@gmail.com>
2021-04-05 18:13:29 +0200

    merged pitchshifter and colordetection into one project

--- colorTrackingPitch/colorTracking.cpp
+++ colorTrackingPitch/colorTracking.cpp
@@ -2,9 +2,106 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
+#include "port_audio.h"
+#include <cmath>
+#include <random>
+#include <iostream>
+#include <string>
+#include "saw.h"
+#include "circBuffer.h"
 
 using namespace cv;
 using namespace std;
+
+//==================================================================================================================
+constexpr double twoPi = 3.14159265359 * 2;
+
+// generates a waveform that will be merged with the sawtooth
+float windowing(float input) {
+    input -= 0.5;
+    input *= 0.5;
+
+    return (float) std::cos(input * twoPi);
+
+}
+
+// the pitcher class returns 2 samples, 1 with a sawtooth and the windowing waveform
+// and 1 which is the same but 180 degrees shifted
+class Pitcher {
+public:
+
+    Pitcher() = default;
+
+    float process(float input) {
+        saw.tick();
+        auto sawSample = saw.getSample();
+
+        circBuffer1.setDistanceRW(sawSample * 4410);
+        circBuffer2.setDistanceRW(std::fmod(sawSample + 0.5, 1) * 4410);
+
+        circBuffer1.write(input);
+        circBuffer2.write(input);
+
+        auto sample1 = circBuffer1.read() * windowing(sawSample);
+        auto sample2 = circBuffer2.read() * windowing(std::fmod(sawSample + 0.5, 1));
+
+        circBuffer1.tick();
+        circBuffer2.tick();
+
+
+        return sample1 + sample2;
+
+
+    }
+
+    Saw saw{4, 44100};
+    CircBuffer circBuffer1{44100};
+    CircBuffer circBuffer2{44100};
+
+};
+
+class NoiseTestCallback : public AudioIODeviceCallback
+{
+public:
+
+    void prepareToPlay (int sampleRate, int blockSize) override
+    {
+        std::cout << "starting callback\n";
+
+    }
+
+    // both channels are added together in the input buffer and mixed with the pitcher class
+    void process (float* input, float* output, int numSamples, int numChannels) override
+    {
+        for (auto sample = 0; sample < numSamples; ++sample)
+        {
+            auto left = input[sample * numChannels];
+            auto right = input[sample * numChannels + 1];
+            auto in = (left + right) / 2;
+            auto out = pitcher.process(in);
+
+            output[sample * numChannels] = out;
+            output[sample * numChannels + 1] = out;
+        }
+    }
+
+    void releaseResources() override
+    {
+        std::cout << "stopping callback\n";
+    }
+
+
+    Pitcher pitcher;
+
+};
+
+// function to change the pitch in the commandline, real change happens at line 77
+auto getNumericInput() {
+    auto ans = std::string {};
+    std::getline(std::cin, ans);
+    return std::stod(ans);
+}
+//=================================================================================================================
 
 // printing green and blue. the last detected color overrules the other color
 class GreenBluePrinter {
@@ -100,7 +197,15 @@ void findColor(Mat object)
 
 int main()
 {
+    auto callback = NoiseTestCallback {};
+    auto portAudio = PortAudio { callback };
+
+    auto sampleRate = 44100;
+    auto blockSize = 64;
+
     VideoCapture cap(0);
+
+    portAudio.setup(sampleRate, blockSize);
 
     while (true)
     {
@@ -114,5 +219,9 @@ int main()
         {
             return 0;
         }
+
+        //callback.pitcher.saw.setFrequency(getNumericInput());
+
     }
+
 }
---
+++ colorTrackingPitch/saw.h
@@ -0,0 +1,16 @@
+
+// Sawtooth formula by Paul Wienk
+
+#pragma once
+
+#include <iostream>
+#include "oscillator.h"
+
+// saw class, derived from base class Oscillator
+class Saw : public Oscillator {
+public:
+    Saw(double frequency, double sampleRate) : Oscillator(frequency, sampleRate) {}
+
+    void tick() override;
+};
+
